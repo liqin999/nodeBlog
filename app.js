95569c85e6a33f619f9155d3e6f1ce6d90d8d241
liqin <1483510623@qq.com>
2017-12-24 15:00:53 +0800

    静态文件托管和模板引擎

---
+++ app.js
@@ -0,0 +1,26 @@
+
+var express = require("express");
+
+//swig是模板引擎，用于加载外部的模板文件
+var swig = require("swig");
+var app = express();
+
+//设置静态文件托管
+//当用户访问的url以/public开始，那么直接返回对应__dirname + '/public'下的文件
+app.use( '/public', express.static( __dirname + '/public') );
+
+//第一个参数：模板引擎的名称，同时也是模板文件的后缀，第二个参数表示用于解析处理模板内容的方法
+app.engine('html',swig.renderFile)
+//设置模板文件存放的目录，第一个参数必须是views，第二个参数是目录
+app.set('views', './views');
+//注册所使用的模板引擎，第一个参数必须是 view engine，第二个参数和app.engine这个方法中定义的模板引擎的名称（第一个参数）是一致的
+app.set('view engine', 'html');
+//在开发过程中，需要取消模板缓存
+swig.setDefaults({cache: false});
+
+//get index.html page
+app.get("/",function(req,res){
+	 res.render('index')
+})
+
+app.listen(8085);
